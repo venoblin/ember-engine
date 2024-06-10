fde5b93b0c83df91fcc8fdc15327fff3fc7bb8fd
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-06-10 00:31:13 -0400

    started work on loging

--- dev_tools/log.h
+++ dev_tools/log.h
@@ -2,6 +2,18 @@
 #define LOG_H
 
 #include <iostream>
+#include <string>
 
+namespace dev_tools 
+{
+  class Log
+  {
+    public:
+      std::string msg;
+      
+      Log() : msg("") {}
+      
+  };
+}
 
 #endif

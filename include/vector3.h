c4b254b25bf5482526ed0051255244af4dabdbe3
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-06-10 00:04:32 -0400

    worked on vector

--- include/vector3.h
+++ include/vector3.h
@@ -3,9 +3,23 @@
 
 namespace engine
 {
-  struct vector3
+  /**
+  * Vector3 data structure with x, y, and z coordinates
+  */
+  struct Vector3
   {
-  
+    public:
+      float x;
+      float y;
+      float z;
+
+      // Default constructor
+      Vector3() : x(0.0f), y(0.0f), z(0.0f) {}
+      // Constructor with arguments 
+      Vector3(float argX, float argY, float argZ) : x(argX), y(argY), z(argZ) {}
+
+      void add(float argX, float argY, float argZ);
+      void subtract(float argX, float argY, float argZ)
   };
 }
 

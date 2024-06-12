58e68a2a79eefe0d494beeced85845087315ce7f
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-06-11 23:33:56 -0400

    worked on main

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
-#include "vector3.h"
+#include <string>
+#include "../include/vector3.h"
 
 int main()
 {
-  Engine::Vector3 vect();
+  Engine::Vector3 vect(19, 0, 29);
+  std::string x = std::to_string(vect.getX());
+  std::string y = std::to_string(vect.getY());
+  std::string z = std::to_string(vect.getZ());
 
-  
+  std::string message = "x: " + x + ", y: " + y + ", z: " + z;
+
+  std::cout << message << std::endl;
 
   return 0;
 }

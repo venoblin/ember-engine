36907208f6c386cc66371ac58e1294d12bfb44da
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-06-13 21:33:00 -0400

    modified build script

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 
 int main()
 {
-  Engine::Vector3 vect;
+  Engine::Vector3 vect(19.0f, 0.0f, 354.5f);
   std::string x = std::to_string(vect.getX());
   std::string y = std::to_string(vect.getY());
   std::string z = std::to_string(vect.getZ());

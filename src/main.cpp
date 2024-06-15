1bbc52c45fbb55b3c06e777314682203f6360fdb
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-06-15 12:50:09 -0400

    added wxwidgets to project

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,8 @@
-#include <iostream>
-#include <string>
-#include "../include/vector3.h"
+
 
 int main()
 {
-  Engine::Vector3 vect(19.0f, 0.0f, 354.5f);
-  std::string x = std::to_string(vect.getX());
-  std::string y = std::to_string(vect.getY());
-  std::string z = std::to_string(vect.getZ());
-
-  std::string message = "x: " + x + ", y: " + y + ", z: " + z;
-
-  std::cout << message << std::endl;
+  
 
   return 0;
 }

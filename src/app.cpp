771919cfa7ac32b2ab26096f3a61d6ea69fda14a
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-06-16 14:27:09 -0400

    added a way to stop app

--- src/app.cpp
+++ src/app.cpp
@@ -8,16 +8,25 @@ App::App() : _isRunning(false) {}
 void App::run()
 {
   setIsRunning(true);
+  std::string answer;
+  
+  std::cout << "Running..." << std::endl;
   
   while (_isRunning)
   {
-    std::cout << "Running..." << std::endl;
+    std::cout << "(q) to quit" << std::endl;
+    std::cin >> answer;
+
+    if (answer == "q") {
+      stop();
+    }
   }
 }
 
 void App::stop()
 {
   std::cout << "Stopped" << std::endl;
+  setIsRunning(false);
 }
 
 void App::setIsRunning(bool b)

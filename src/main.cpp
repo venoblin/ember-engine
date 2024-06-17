e3ec6cf7cfd05899d55a006295e6a9ad64f4281b
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-06-16 21:57:47 -0400

    added sdl starting point

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,41 @@
+#include <SDL2/SDL.h>
 
 int main()
 {
+  SDL_Window* window = SDL_CreateWindow(
+    "Game Engine", 
+    SDL_WINDOWPOS_UNDEFINED, 
+    SDL_WINDOWPOS_UNDEFINED, 
+    640, 480, 
+    SDL_WINDOW_SHOWN
+  );
+
+  if (window == nullptr) {
+    SDL_Log("Window could not be created! SDL Error: %s", SDL_GetError());
+    SDL_Quit();
+    return 1;
+  }
+
+  SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+  if (renderer == nullptr) {
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 1;
+  }
+
+  bool quit = false;
+  SDL_Event event;
+  while (!quit) {
+    while (SDL_PollEvent(&event)) {
+      if (event.type == SDL_QUIT) {
+        quit = true;
+      }
+    }
+  }
+
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+
   return 0;
 }

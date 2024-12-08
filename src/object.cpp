a0e77a1cd1fa8eacca7fdcb708c6d677ea9859a1
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-12-08 01:39:29 -0500

    worked on object

--- src/object.cpp
+++ src/object.cpp
@@ -1,7 +1,12 @@
 #include "object.h"
 
-Object::Object(int width, int height, float x, float y, float z) 
-: width(width), height(height), x(x), y(y), z(z) {}
+Object::Object(int width = 20, int height = 20, float x = 0, float y = 0, float z = 0) 
+: width(width), height(height), x(x), y(y), z(z) {
+  this->collider.w = width;
+  this->collider.h = height;
+  this->collider.x = x;
+  this->collider.y = y;
+}
 
 void Object::setSize(int width, int height) {
   this->width = width;
--- src/object.h
+++ src/object.h
@@ -1,6 +1,8 @@
 #ifndef OBJECT_H
 #define OBJECT_H
 
+#include <SDL2/SDL.h>
+
 class Object {
   private:
     int width;
@@ -8,6 +10,8 @@ class Object {
     float x;
     float y;
     float z;
+    SDL_Rect collider;
+    
   public:
     Object(int width, int height, float x, float y, float z);
 

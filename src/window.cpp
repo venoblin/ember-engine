31247c081458fdde8ad5247cb1408bfcde8fda82
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-12-08 11:31:57 -0500

    created window files

---
+++ src/window.cpp
@@ -0,0 +1,3 @@
+#include "window.h"
+
+Window::Window(const char* title, int width, int height) : title(title), width(width), height(height) {}
---
+++ src/window.h
@@ -0,0 +1,18 @@
+#ifndef WINDOW_H
+#define WINDOW_H
+
+#include <SDL2/SDL.h>
+
+class Window {
+  private:
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    int sdlInit;
+    const char* title;
+    int width;
+    int height;
+  public:
+    Window(const char* title, int width, int height);
+};
+
+#endif

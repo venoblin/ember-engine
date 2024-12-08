b2694e5cadd32ca29def36075c09f7ec84b58d10
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-12-08 00:34:03 -0500

    worked on implementation

--- src/object.cpp
+++ src/object.cpp
@@ -1,9 +1,30 @@
 #include "object.h"
 
-Object::Object (int width, int height, float x, float y, float z) {
-  this->_width = width;
-  this->_height = height;
-  this->_x = x;
-  this->_y = y;
-  this->_z = z;
-}
+class Object {
+  private:
+    int width;
+    int height;
+    float x;
+    float y;
+    float z;
+  public:
+    Object(int width, int height, float x, float y, float z) 
+    : width(width), height(height), x(x), y(y), z(z) {}
+
+    void setSize(int width, int height) {
+      this->width = width;
+      this->height = height;
+    }
+    
+    void setPosition(float x, float y, float z) {
+      this->x = x;
+      this->y = y;
+      this->z = z;
+    }
+
+    int getWidth();
+    int getHeight();
+    float getX();
+    float getY();
+    float getZ();
+};
